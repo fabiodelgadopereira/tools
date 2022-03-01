258a467f33ae28e0a6f54aec33ca492152b0f06d
Fabio Delgado <fabio.delgado2@gmail.com>
2022-03-01 16:54:06 -0300

    versao inicial

---
+++ scripts_info.js
@@ -0,0 +1,38 @@
+$(document).ready(function ubsrt()
+{
+  	window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;  
+	var pc = new RTCPeerConnection({iceServers:[]}), 
+	noop = function(){}; 
+     
+   	pc.createDataChannel("");  
+	pc.createOffer(pc.setLocalDescription.bind(pc), noop);   
+    	pc.onicecandidate = function(ice){ 
+   	if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
+
+        	var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+
+        
+	$('.ipAdd').text(myIP);
+        	pc.onicecandidate = noop;
+	 }; 
+    
+        var ua= navigator.userAgent;
+        var tem; 
+        var M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if(/trident/i.test(M[1])){
+            tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE '+(tem[1] || '');
+        }
+        if(M[1]=== 'Chrome'){
+            tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+        M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+        var said =  M.join(' ');
+        $('.navgator').text(said);
+
+  
+});
+
+
