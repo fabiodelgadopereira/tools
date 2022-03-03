82f929045bfb6a7567e00001b1ae7f7048ed6869
Fabio Delgado <fabio.delgado2@gmail.com>
2022-03-02 23:07:51 -0300

    formatando codigo

--- scripts_info.js
+++ scripts_info.js
@@ -1,38 +1,36 @@
 document.onload = myInfo()
 
 function myInfo() {
-  window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;//compatibility for Firefox and chrome
-var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};      
-pc.createDataChannel('');//create a bogus data channel
-pc.createOffer(pc.setLocalDescription.bind(pc), noop);// create offer and set local description
-pc.onicecandidate = function(ice)
-{
- if (ice && ice.candidate && ice.candidate.candidate)
- {
-  var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
-  document.getElementById("myIp").innerHTML = myIP;  
-  pc.onicecandidate = noop;
- }
-};
- 
-    
-        var ua= navigator.userAgent;
-        var tem; 
-        var M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-        if(/trident/i.test(M[1])){
-            tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-            return 'IE '+(tem[1] || '');
+    window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for Firefox and chrome
+    var pc = new RTCPeerConnection({
+            iceServers: []
+        }),
+        noop = function() {};
+    pc.createDataChannel(''); //create a bogus data channel
+    pc.createOffer(pc.setLocalDescription.bind(pc), noop); // create offer and set local description
+    pc.onicecandidate = function(ice) {
+        if (ice && ice.candidate && ice.candidate.candidate) {
+            var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+            document.getElementById("myIp").innerHTML = myIP;
+            pc.onicecandidate = noop;
         }
-        if(M[1]=== 'Chrome'){
-            tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-            if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-        }
-        M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-        if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-        var said =  M.join(' ');
-        document.getElementById("navgator").innerHTML = said;
-      
+    };
+
+
+    var ua = navigator.userAgent;
+    var tem;
+    var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if (/trident/i.test(M[1])) {
+        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE ' + (tem[1] || '');
+    }
+    if (M[1] === 'Chrome') {
+        tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+    var said = M.join(' ');
+    document.getElementById("navgator").innerHTML = said;
+
 }
-
-
-
--- scripts_pdf.js
+++ scripts_pdf.js
@@ -1,28 +1,27 @@
-
 async function mergeAllPDFs() {
 
-  var selectedFile = document.getElementById("inputFile").files;
-  const pdfDoc = await PDFLib.PDFDocument.create();
-  const numDocs = 2;
-  
-  for(var i = 0; i < numDocs; i++) {
-    let reader = await readFileAsync(selectedFile[i]);
-    
-      const donorPdfDoc = await PDFLib.PDFDocument.load(reader);
-      const docLength = donorPdfDoc.getPageCount();
-      for(var k = 0; k < docLength; k++) {
-          const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [k]);
-          //console.log("Doc " + i+ ", page " + k);
-          pdfDoc.addPage(donorPage);
-      }
-  }
+    var selectedFile = document.getElementById("inputFile").files;
+    const pdfDoc = await PDFLib.PDFDocument.create();
+    const numDocs = 2;
 
-  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
- 
+    for (var i = 0; i < numDocs; i++) {
+        let reader = await readFileAsync(selectedFile[i]);
+        const donorPdfDoc = await PDFLib.PDFDocument.load(reader);
+        const docLength = donorPdfDoc.getPageCount();
+        for (var k = 0; k < docLength; k++) {
+            const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [k]);
+            //console.log("Doc " + i+ ", page " + k);
+            pdfDoc.addPage(donorPage);
+        }
+    }
+    const pdfDataUri = await pdfDoc.saveAsBase64({
+        dataUri: true
+    });
 
-  // strip off the first part to the first comma "data:image/png;base64,iVBORw0K..."
- // var data_pdf = pdfDataUri.substring(pdfDataUri.indexOf(',')+1);
-  downloadURI(pdfDataUri, "output.pdf");
+
+    // strip off the first part to the first comma "data:image/png;base64,iVBORw0K..."
+    // var data_pdf = pdfDataUri.substring(pdfDataUri.indexOf(',')+1);
+    downloadURI(pdfDataUri, "output.pdf");
 }
 
 
@@ -34,42 +33,44 @@ function downloadURI(uri, name) {
     link.click();
     document.body.removeChild(link);
     delete link;
-  }
-  function downloadFunction() {
+}
+
+function downloadFunction() {
     //"data:text/html para text
-    var conteudo = "data:application/pdf;base64,"+document.getElementById('inputDecode').value;
-   var nomeArquivo = document.getElementById('fname').value+".pdf";
-  downloadURI(conteudo, nomeArquivo);
-  
+    var conteudo = "data:application/pdf;base64," + document.getElementById('inputDecode').value;
+    var nomeArquivo = document.getElementById('fname').value + ".pdf";
+    downloadURI(conteudo, nomeArquivo);
+
 }
 
 function readFileAsync(file) {
     return new Promise((resolve, reject) => {
-       let reader = new FileReader(); 
-       reader.onload = () => {
-          resolve(reader.result);
-       }; 
-       reader.onerror = reject; 
-       reader.readAsArrayBuffer(file);
+        let reader = new FileReader();
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsArrayBuffer(file);
     })
- }
+}
+
 function convertToBase64() {
-  //Read File
-  var selectedFile = document.getElementById("inputFile").files;
-  //Check File is not Empty
-  if (selectedFile.length > 0) {
-      // Select the very first file from list
-      var fileToLoad = selectedFile[0];
-      // FileReader function for read the file.
-      var fileReader = new FileReader();
-      var base64;
-      // Onload of file read the file content
-      fileReader.onload = function(fileLoadedEvent) {
-          base64 = fileLoadedEvent.target.result;
-          // Print data in console
-          //console.log(base64);
-      };
-      // Convert data to base64
-      fileReader.readAsDataURL(fileToLoad);
-  }
+    //Read File
+    var selectedFile = document.getElementById("inputFile").files;
+    //Check File is not Empty
+    if (selectedFile.length > 0) {
+        // Select the very first file from list
+        var fileToLoad = selectedFile[0];
+        // FileReader function for read the file.
+        var fileReader = new FileReader();
+        var base64;
+        // Onload of file read the file content
+        fileReader.onload = function(fileLoadedEvent) {
+            base64 = fileLoadedEvent.target.result;
+            // Print data in console
+            //console.log(base64);
+        };
+        // Convert data to base64
+        fileReader.readAsDataURL(fileToLoad);
+    }
 }
